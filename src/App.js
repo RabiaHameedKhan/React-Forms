de95e4ffdc35c8b5b5e0bcda0618f2a9f788f2cd
RabiaHameedKhan <rabiahameedkhan27@gmail.com>
2025-04-29 21:04:49 +0500

    updated

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const App = () => { //controlled component
+
+  const [name, setname]= useState("");
+  const [fullName, setfullname]= useState("");
+
+
+  const Event=(event)=>{
+    
+      console.log(event.target.value)
+      setname (event.target.value)
+   
+  }
+
+  const onSubmit=(event)=>{
+    setfullname(name)
+    event.preventDefault(); // prevents page reload
+  }
+  return (
+    <div>
+      <h1>Welcome to My React Form</h1>
+      <h1>Hi {fullName}</h1>
+      <form>
+        <input placeholder="Enter your name" onChange={Event} value={name}/>
+        
+        <button type="submit" onClick={onSubmit}>Submit</button>
+      </form>
+    </div>
+  );
+}; 
+
+export default App;
